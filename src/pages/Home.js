6389f725e1e25c7347c9911f089d59589dd4e6fe
yaniamiatun21 <yaniamiatun21@gmail.com>
2024-03-06 16:52:51 +0700

    tambah function umur

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,23 @@ export default function Home() {
   useEffect(function () {
     document.title = `Home`;
   }, []);
+
+  function calculateAge(yearOfBirth) {
+    var currentYear = new Date().getFullYear();
+    var age = currentYear - yearOfBirth;
+    return age;
+  }
+
+  // Example usage:
+  var yearOfBirth = 2002;
+  var age = calculateAge(yearOfBirth);
+
   return (
     <section className="section">
       <h1 className="section-title">Selamat Datang</h1>
       <p className="section-description">
-        Halo semua, nama saya yani ami. Ini website baru aku. Kamu bisa
-        mendapatkan informasi seputar profileku disini. Salam kenal ya semua.
+        Halo semua, nama saya yani ami, umur {age} tahun. Ini website baru aku.
+        Kamu bisa mendapatkan informasi seputar profileku disini.
       </p>
     </section>
   );
